3e419a6a6af06a506e308c30bd8a5bcef95004dc
Levi de los Santos <levidlsantos@gmail.com>
2020-06-02 01:54:33 -0300

    Test Cases added

---
+++ src/Test/Library.Test/GeneradorPerfil/GeneracionPerfilStub.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Library;
+
+namespace Library.Test
+{
+    internal class GeneradorPerfilStub: Library.GeneradorPerfil
+    {
+
+        public GeneradorPerfilStub() 
+        {
+            
+        }
+      
+
+
+
+
+
+    }
+}
